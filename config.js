293bc088c1093bb0c6c28e7e395429f5543c9a74
chaimete-debug <chaimete@gmail.com>
2026-05-31 22:56:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // 1) Publique backend/Code.gs como Google Apps Script Web App.
 // 2) Substitua a URL abaixo pela URL terminada em /exec.
 window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzvzwAPbEU_Ar-8zUeDLcGpYl3gBXONvzU4AVDsSgvFIAhfDWn_sfx6qqdlSn2my3aKzw/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxwrmHUJwZAHRuCIwQwdxv0WImu1CnVNAGdQOMtmFiaDEf5_bSRbkaNV03edzCe1bD3/exec,
   APP_NAME: 'Gestão Digital da Igreja',
   ORG_NAME: 'Igreja Local',
   DEFAULT_PIN_ENABLED: false
