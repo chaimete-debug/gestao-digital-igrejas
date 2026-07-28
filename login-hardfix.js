3438df8a612b3b58cf93c9727c274076b4394aeb
chaimete-debug <chaimete@gmail.com>
2026-07-28 09:09:18 +0200

    Delete login-hardfix.js

--- login-hardfix.js
+++
@@ -1,7 +0,0 @@
-/*
- * Ficheiro legado da v53.
- * Desde a v54.1.1 já não é carregado por index.html.
- * O login é tratado exclusivamente por setupLoginHandlers() em app.js,
- * evitando submissões duplicadas ao clicar em Entrar ou premir Enter.
- */
-console.info('login-hardfix.js legado: não deve ser incluído no index.html a partir da v54.1.1.');
